2031a5eb6460c256b5b302f375c8df7945bd49c9
Berci <bertalan.pecsi@nokia.com>
2015-11-10 21:09:24 +0100

    ujson: oneliners for json Read/Write

--- util/__init__.py
+++ util/__init__.py
@@ -1,17 +1,25 @@
-#######################################
-#
-#  utility stuff: 
-#   - Symbols, 
-#   - processing a sequence of tasks
-#   - @buildOnObject decorator
-#   - ShiftedList and CycleList
-#
-#######################################
+'''
+   utility stuff: 
+    - lists: 
+        ShiftedList and CycleList
+    - baseObject: 
+        @buildOnObject decorator
+    - wp:
+        @withoutParenthesis decorator for cmd line usage of a function without parenthesis
+    - pr2:
+        PrintTo context
+    - ujson
+        oneliners for json Read/Write
+    - symbols
+        function Symbols to define symbols into preset namespace
+
+'''
 
 import lists
 import baseObject
 import wp
 import pr2
+import ujson
 
 import symbols
 symbols.addSymbolsTo(globals())  # Symbols created at util pkg level
